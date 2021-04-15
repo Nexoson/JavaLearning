33adf812ddbf4303b3fe8d6989df9f5c71a48c30
黄迅 <huangxun888888@gmail.com>
2021-04-15 18:56:50 +0800

    feat: JavaPlus初始化上传

---
+++ JavaPlus/src/test/java/com/max/javaplus/JavaPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.javaplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
