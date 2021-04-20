9fb6a1bdf41dacebc40725eab67c1ee37b7ac95f
黄迅 <huangxun888888@gmail.com>
2021-04-20 21:53:41 +0800

    feat: 新增Java8 Stream学习, 规约部分代码完成, 目前笔记待补全(图片格式统一)

--- Java8/src/main/java/com/max/java8/stream/chapter06/StreamReduceDemo.java
+++ Java8/src/main/java/com/max/java8/stream/chapter06/StreamReduceDemo.java
@@ -34,7 +34,11 @@ public class StreamReduceDemo {
         // 求最大值方式1
         Optional<Integer> max = list.stream().reduce((x, y) -> x > y ? x : y);
         // 求最大值方式2
-        Integer max2 = list.stream().reduce(1, Integer::max);
+        Integer max2 = list.stream().reduce(10, Integer::max);
+        /**
+         * 个人理解 identity可作为锚点, 也是参与运算符运算的元素之一,
+         * 如求最大值方式2中identity=12的话,则最大值就为12 否则就是11
+         **/
         System.out.println("list求和：" + sum.get() + "," + sum2.get() + "," + sum3);
         System.out.println("list求积：" + product.get());
         System.out.println("list求最大值：" + max.get() + "," + max2);
@@ -50,6 +54,10 @@ public class StreamReduceDemo {
         // 求工资之和方式3
         Integer sumSalary3 = personList.stream().reduce(0, (sum, p) -> sum += p.getSalary(), Integer::sum);
         // 求最高工资方式1
+        Integer maxSalary = personList.stream().reduce(0, (max, p) -> max > p.getSalary() ? max : p.getSalary(), Integer::max);
         // 求最高工资方式2
+        Integer maxSalary2 = personList.stream().reduce(0, (max, p) -> max > p.getSalary() ? max : p.getSalary(), (a, b) -> a > b ? a : b);
+        System.out.println("工资之和：" + sumSalary.get() + "," + sumSalary2 + "," + sumSalary3);
+        System.out.println("最高工资：" + maxSalary + "," + maxSalary2);
     }
 }
