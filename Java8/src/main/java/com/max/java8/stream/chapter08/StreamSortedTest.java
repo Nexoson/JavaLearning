30f3646d042ea5e9c7143f5222099094ec02da55
Nexoson <1018458278@qq.com>
2021-04-22 23:17:40 +0800

    feat: Java8 Stream学习 Sorted 基础用法完结

--- Java8/src/main/java/com/max/java8/stream/chapter08/StreamSortedTest.java
+++ Java8/src/main/java/com/max/java8/stream/chapter08/StreamSortedTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author huangX huangxun@lidomtech.com
@@ -49,7 +50,29 @@ public class StreamSortedTest {
         System.out.println("按工资降序排序：" + newList2);
         System.out.println("先按工资再按年龄升序排序：" + newList3);
         System.out.println("先按工资再按年龄自定义降序排序：" + newList4);
-
     }
 
+
+    /**
+     * 提取/组合
+     * 流也可以进行合并、去重、限制、跳过等操作
+     **/
+    @Test
+    public void test02() {
+        String[] arr1 = {"a", "b", "c", "d"};
+        String[] arr2 = {"d", "e", "f", "g"};
+
+        Stream<String> stream1 = Stream.of(arr1);
+        Stream<String> stream2 = Stream.of(arr2);
+
+        // concat:合并两个流  distinct：去重
+        List<String> newList = Stream.concat(stream1, stream2).distinct().collect(Collectors.toList());
+        // limit:限制从流中获取n个数据
+        List<Integer> collect = Stream.iterate(1, x -> x + 2).limit(10).collect(Collectors.toList());
+        // skip:跳过前n个数据
+        List<Integer> collect2 = Stream.iterate(1, x -> x + 3).skip(1).limit(5).collect(Collectors.toList());
+        System.out.println("流合并：" + newList);
+        System.out.println("limit：" + collect);
+        System.out.println("skip：" + collect2);
+    }
 }
