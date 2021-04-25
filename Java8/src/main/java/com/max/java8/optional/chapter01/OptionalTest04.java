0ba906f15bbb38ac334557b7fb40646e40447f4a
黄迅 <huangxun888888@gmail.com>
2021-04-25 14:55:25 +0800

    feat: Optional学习 isPresent()方法

--- Java8/src/main/java/com/max/java8/optional/chapter01/OptionalTest04.java
+++ Java8/src/main/java/com/max/java8/optional/chapter01/OptionalTest04.java
@@ -2,17 +2,29 @@ package com.max.java8.optional.chapter01;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 /**
  * @author huangX huangxun@lidomtech.com
  * @version 1.0
  * @className OptionalTest04
  * @date 2021/4/23 19:57
- * @desc isPresent()和ifPresent(Consumer<? super T> consumer)
+ * @desc isPresent()和ifPresent(Consumer < ? super T > consumer)
  **/
 public class OptionalTest04 {
 
     @Test
     public void test01() {
+
+        User user = new User();
+        user = null;
+
+        boolean isPresent = Optional.ofNullable(user).isPresent();
+        System.out.println("user 不为空: " + isPresent);
+
+        Optional.ofNullable(user).ifPresent(u -> {
+            System.out.println("ifPresent判断 user不为空");
+        });
 
     }
 }
