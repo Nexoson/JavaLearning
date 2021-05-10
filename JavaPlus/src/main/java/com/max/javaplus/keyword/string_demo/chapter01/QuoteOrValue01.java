f358d7d3b5b1482e269a29340145847b7545017c
黄迅 <huangxun888888@gmail.com>
2021-05-10 10:26:58 +0800

    feat: Java 值传递 探秘

--- JavaPlus/src/main/java/com/max/javaplus/keyword/string_demo/chapter01/QuoteOrValue01.java
+++ JavaPlus/src/main/java/com/max/javaplus/keyword/string_demo/chapter01/QuoteOrValue01.java
@@ -8,25 +8,25 @@ import org.openjdk.jol.info.GraphLayout;
  * @version 1.0
  * @className QuoteOrValue01
  * @date 2021/5/7 14:34
- * @desc
+ * @desc Java 值传递 探秘
  **/
 public class QuoteOrValue01 {
 
     public static void main(String[] args) {
         String a = new String("a");
-        System.out.println("对象内部: " + GraphLayout.parseInstance(a).toPrintable());
-        System.out.println("对象外部: " + GraphLayout.parseInstance("a").toPrintable());
+        System.out.println("对象: " + GraphLayout.parseInstance(a).toPrintable());
+        System.out.println("对象: " + GraphLayout.parseInstance("a").toPrintable());
         a = "b";
-        System.out.println("对象内部: " + GraphLayout.parseInstance(a).toPrintable());
-        System.out.println("对象外部: " + GraphLayout.parseInstance("b").toPrintable());
+        System.out.println("对象: " + GraphLayout.parseInstance(a).toPrintable());
+        System.out.println("对象: " + GraphLayout.parseInstance("b").toPrintable());
         //changeValue(a);
         System.out.println(a);
     }
 
     public static void changeValue(String temp) {
         String a = new String("a");
-        System.out.println("对象内部: " + ClassLayout.parseInstance(a).toPrintable());
-        System.out.println("对象外部: " + GraphLayout.parseInstance("a").toPrintable());
+        System.out.println("对象: " + ClassLayout.parseInstance(a).toPrintable());
+        System.out.println("对象: " + GraphLayout.parseInstance("a").toPrintable());
         a += "a";
 
 
