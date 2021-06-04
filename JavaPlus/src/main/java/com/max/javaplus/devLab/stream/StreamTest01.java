3b8191a4ec196254a39dfb0fe000c341a5d47106
黄迅 <huangxun888888@gmail.com>
2021-06-04 10:51:34 +0800

    feat: 验证空集合调用stream场景

---
+++ JavaPlus/src/main/java/com/max/javaplus/devLab/stream/StreamTest01.java
@@ -0,0 +1,21 @@
+package com.max.javaplus.devLab.stream;
+
+import org.junit.Test;
+
+/**
+ * @author huangX huangxun@lidomtech.com
+ * @version 1.0
+ * @className StreamTest01
+ * @date 2021/6/4 10:14
+ * @desc
+ **/
+public class StreamTest01 {
+
+    /**
+     * 验证空集合调用stream场景
+     **/
+    @Test
+    public void test01() {
+
+    }
+}
