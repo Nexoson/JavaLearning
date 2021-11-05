7e6743ae441e27dffab1b2f5a5b3a3299eeae183
黄迅 <huangxun888888@gmail.com>
2021-11-05 13:59:43 +0800

    feat: 设计模式, 抽象工厂方法模式

---
+++ java-plus/src/main/java/com/max/javaplus/design_pattern/factory_pattern/abstract_factory/HaweiPhone.java
@@ -0,0 +1,30 @@
+package com.max.javaplus.design_pattern.factory_pattern.abstract_factory;
+
+/**
+ * @author huangX huangxun@lidomtech.com
+ * @version 1.0
+ * @className MiPhone
+ * @date 2021/11/5 11:45
+ * @desc 华为手机
+ **/
+public class HaweiPhone implements IPhoneProduct {
+    @Override
+    public void powerOn() {
+        System.out.println("华为手机开机");
+    }
+
+    @Override
+    public void powerOff() {
+        System.out.println("华为手机关机");
+    }
+
+    @Override
+    public void call() {
+        System.out.println("华为手机打电话");
+    }
+
+    @Override
+    public void seanSMS() {
+        System.out.println("华为手机发短信");
+    }
+}
---
+++ java-plus/src/main/java/com/max/javaplus/design_pattern/factory_pattern/abstract_factory/HaweiRoute.java
@@ -0,0 +1,15 @@
+package com.max.javaplus.design_pattern.factory_pattern.abstract_factory;
+
+/**
+ * @author huangX huangxun@lidomtech.com
+ * @version 1.0
+ * @className MiRoute
+ * @date 2021/11/5 11:53
+ * @desc 华为路由器
+ **/
+public class HaweiRoute implements IRouteProduct {
+    @Override
+    public void openWifi() {
+        System.out.println("华为1000M路由器");
+    }
+}
