1bccbf0bd77a6a17ea386b3709e5ff98ad87c4d5
黄迅 <huangxun888888@gmail.com>
2021-11-05 11:07:06 +0800

    feat: 设计模式, 简单工厂模式(静态工厂模式)

---
+++ java-plus/src/main/java/com/max/javaplus/design_pattern/factory_pattern/simple_factory/Consumer.java
@@ -0,0 +1,31 @@
+package com.max.javaplus.design_pattern.factory_pattern.simple_factory;
+
+/**
+ * @author huangX huangxun@lidomtech.com
+ * @version 1.0
+ * @className Consumer
+ * @date 2021/11/5 10:45
+ * @desc 消费者买车
+ **/
+public class Consumer {
+
+    public static void main(String[] args) {
+
+        // 接口,实现类创建
+        System.out.println("==============类创建===============");
+
+        Car wuling = new WuLing();
+        wuling.getName();
+
+        Car tesla = new Tesla();
+        tesla.getName();
+
+        // 用工厂创建
+        System.out.println("============静态工厂创建==============");
+        Car c1 = CarFactory.getCar("五菱");
+        c1.getName();
+        Car c2 = CarFactory.getCar("特斯拉");
+        c2.getName();
+
+    }
+}
